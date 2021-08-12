5c9d89ea1ef652e99d13fff282f828e8331a6d2b
Marcelo Marques <marcelo@smarques.com>
2021-08-11 21:27:51 -0300

    Change Instance Name

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ provider "google" {
 }
 
 resource "google_compute_instance" "vm_instance" {
-  name         = "terraform-instance"
+  name         = "app-instance"
   machine_type = "f1-micro"
 
   boot_disk {
